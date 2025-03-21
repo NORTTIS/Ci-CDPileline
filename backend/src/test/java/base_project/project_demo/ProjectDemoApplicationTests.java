2159e1726f8ecbb1adc9c0dee961f3dc0a502204
Norttisk <2132k4@gmail.com>
2025-03-21 13:31:30 +0700

    Add ActiveProfiles annotation to ProjectDemoApplicationTests for test profile

--- backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
+++ backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
@@ -2,12 +2,15 @@ package base_project.project_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        System.out.println("Active Profile: " + System.getProperty("spring.profiles.active"));
+    }
 
 }
