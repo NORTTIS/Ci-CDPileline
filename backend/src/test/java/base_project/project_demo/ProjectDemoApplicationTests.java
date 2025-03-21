74c7638081eabe1c3292a8f12122b012905d3f8e
Norttisk <2132k4@gmail.com>
2025-03-21 15:41:26 +0700

    Remove H2 database dependency and simplify ProjectDemoApplicationTests

--- backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
+++ backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
@@ -1,9 +1,7 @@
 package base_project.project_demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProjectDemoApplicationTests {
 
     @Test
