c6da492bcddb5f8ce0b181089e7d705a229bc62b
Norttisk <2132k4@gmail.com>
2025-03-21 13:40:33 +0700

    Remove unused test properties and clean up ProjectDemoApplicationTests

--- backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
+++ backend/src/test/java/base_project/project_demo/ProjectDemoApplicationTests.java
@@ -5,12 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ProjectDemoApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("Active Profile: " + System.getProperty("spring.profiles.active"));
     }
 
 }
